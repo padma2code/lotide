322d592b9078777642d053e77c5862a6ab274275
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-05 03:23:31 +0000

    adding tail.js

---
+++ tail.js
@@ -0,0 +1,39 @@
+let res= []
+let tail = function (params)
+{
+    for (i=1; i<params.length; i++) {
+    res.push(params[i])
+    
+    }
+    return res
+}
+
+
+const assertEqual = function(words, expected) {
+
+    console.log (words)
+    console.log (expected)
+    if (words.toString() === expected.toString()) {
+        
+        console.log('✅️✅️✅️Assertion Passed:'+ words + '===' + expected);
+    } else {
+        console.log('🔴️🔴️🔴️Assertion Failed:'+ words + '!=' + expected);
+    }
+  };
+
+//const words = tail(["Hello", "Lighthouse", "Labs"]);
+//assertEqual(words, ["Lighthouse", "Labs"]); // => will pass!
+
+
+//const result = tail(["Hello", "Lighthouse", "Labs"]);
+//assertEqual(result.length,  2); // ensure we get back two elements
+//assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+//assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); // => will always fail!
+
+const word = ["Yo Yo", "Lighthouse", "Labs"];
+//console.log(tail(word).length); // no need to capture the return value since we are not checking it
+//assertEqual(word.length, 3); // original array should still have 3 elements!
+assertEqual(tail(word).length, 3); 
+
