64197d8706f8f2a6f305fb5837709d45804384ec
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-05 05:08:40 +0000

    adding strech project files

---
+++ reverse.js
@@ -0,0 +1,32 @@
+
+let reverse = function(params) {
+  let res = [];
+  for (let i = params.length - 1; i >= 0; i--) {
+    res += params[i];
+  }
+  return res;
+};
+
+
+for (let j = 2; j < process.argv.length; j++) {
+  console.log(reverse(process.argv[j]));
+}
+
+
+/* this is the program to give inline input and not in runtime
+let reverse = function(params) {
+    let res = [];
+      for (let i = params.length-1; i >=0; i--) {
+        res += params[i]
+      }
+      return res;
+    };
+    
+    let arr = ["valli", "dutt", "abcd"]
+    for (let j = 0; j <= arr.length-1; j++) {
+       
+        console.log(reverse(arr[j]))
+    };
+    */
+
+
