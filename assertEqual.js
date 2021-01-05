a5912bc5646d622e38f0920874ee583ec283ff74
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-05 00:33:12 +0000

    Assertequal

---
+++ assertEqual.js
@@ -0,0 +1,29 @@
+/*console.assert(1 === 1);
+console.assert(1 === 1.1);
+
+const sum = function(a, b) {
+    return a + b;
+  }
+  
+  // TEST CODE
+  console.assert(sum(1, 2) === 3);
+  console.assert(sum(1, 20) === 3);
+  
+  const sumBuggy = function(a, b) {
+    return a * b;
+  }
+  
+  // TEST CODE
+  console.assert(sumBuggy(1, 2) === 2);*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅️✅️✅️Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("🔴️🔴️🔴️Assertion Failed: [actual] != [expected]");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
