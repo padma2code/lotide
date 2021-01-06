ececc89c08602eec48d6ec6576a54becad0bf709
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-06 23:30:24 +0000

    stringletters count function

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+const getFrequency = (string) => {
+  let freq = {};
+  for (let i = 0; i < string.length; i++) {
+    let character = string.charAt(i);
+    if (freq[character]) {
+      freq[character]++;
+    } else {
+      freq[character] = 1;
+    }
+  }
+
+  return freq;
+};
+console.log(getFrequency("abbsdssr"));
