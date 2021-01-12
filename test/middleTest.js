04eb58ba0bc8a85c83556c842aa2f09a0a672c74
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-12 01:52:11 +0000

    refactor middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+
+const assertEqual =require('../assertEqual');
+const eqArrays =require('../eqArrays');
+const middle=require('../middle');
+
+console.log(middle([1,2,3]));
+console.log(middle([1,2,3,4]));
